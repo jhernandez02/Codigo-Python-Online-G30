8147fa5b6bb6b9411dfc9dc0df1071bd5e8a25df
12aptor <paholo2054@gmail.com>
2026-03-18 22:58:14 -0500

    Flask con postgresql

--- Tema05 - Flask/Clase13_18-03-2026/api-flask/app.py
+++ Tema05 - Flask/Clase13_18-03-2026/api-flask/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -6,10 +6,34 @@ app = Flask(__name__)
 def home():
     return 'Hola, bienvenido a mi API Flask! 🐍'
 
-@app.route('/users', methods=["GET", "POST", "PUT", "DELETE"])
+@app.route('/user', methods=["GET", "POST", "PUT", "DELETE"])
 def users():
-    return 'Usuarios'
+    method = request.method
 
+    if method == "GET":
+        return {
+            'id': 1,
+            'name': 'Jhon Doe',
+            'email': 'jhon@example.com',
+        }
+    elif method == "POST":
+        json = request.get_json()
+        return json
+
+@app.route('/users/<name>') # <int:user_id>, <string:name>, <float:price>, <path:path>, <uuid:uuid>
+def user(name):
+    return f'Hola {name}'
+
+@app.route('/users/<int:user_id>', methods=["PUT"])
+def update_user(user_id):
+    json = request.get_json()
+    name = json.get('name')
+    email = json.get('email')
+    return {
+        'id': user_id,
+        'name': name,
+        'email': email
+    }
 
 if __name__ == '__main__':
     app.run(debug=True)
