810f3a60831385691c775312572f36cdf69f4eec
12aptor <paholo2054@gmail.com>
2026-03-23 21:37:34 -0500

    Autenticación con Flask

--- Tema05 - Flask/Clase15_23-03-2026/flask-auth/app.py
+++ Tema05 - Flask/Clase15_23-03-2026/flask-auth/app.py
@@ -5,7 +5,7 @@ import bcrypt
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = ''
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:root@localhost:5432/flask-auth'
 
 db = SQLAlchemy()
 
@@ -19,24 +19,43 @@ class User(db.Model):
 with app.app_context():
     db.create_all()
 
-@app.route('/users', methods=['POST'])
+@app.route('/users', methods=['POST', 'GET'])
 def users():
     method = request.method
-    if method == 'POST':
-        data = request.get_json()
+    try:
+        if method == 'POST':
+            data = request.get_json()
 
-        user = User.query.filter_by(
-            email=data.get('email')
-        ).first()
+            user = User.query.filter_by(
+                email=data.get('email')
+            ).first()
 
-        if user:
-            raise Exception('User already exists')
-        
-        User(
-            name=data.get('name'),
-            email=data.get('email'),
-            password=''
-        )
+            if user:
+                raise Exception('User already exists')
+            
+
+            bytes_password = data.get('password').encode('utf-8')
+            hashed_password = bcrypt.hashpw(bytes_password, bcrypt.gensalt())
+            string_hashed_password = hashed_password.decode('utf-8')
+            
+            created_user = User(
+                name=data.get('name'),
+                email=data.get('email'),
+                password=string_hashed_password
+            )
+
+            db.session.add(created_user)
+            db.session.commit()
+
+            return {
+                'id': created_user.id,
+                'name': created_user.name,
+                'email': created_user.email
+            }
+    except Exception as e:
+        return {
+            'message': str(e)
+        }, 400
 
 if __name__ == '__main__':
     app.run(debug=True)
