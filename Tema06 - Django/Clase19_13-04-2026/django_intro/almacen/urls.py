c99a1be865196ebef5978fe4f01d82b5e7d24eee
12aptor <paholo2054@gmail.com>
2026-04-15 20:12:12 -0500

    Introducción a Django

--- Tema06 - Django/Clase19_13-04-2026/django_intro/almacen/urls.py
+++ Tema06 - Django/Clase19_13-04-2026/django_intro/almacen/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
-from .views import index
+from .views import (
+    home,
+    get_date,
+    get_json,
+    get_html,
+)
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('', home, name='home'),
+    path('date/', get_date, name='get_date'),
+    path('json/', get_json, name='get_json'),
+    path('html/', get_html, name='get_html'),
 ]
--- Tema06 - Django/Clase19_13-04-2026/django_intro/almacen/views.py
+++ Tema06 - Django/Clase19_13-04-2026/django_intro/almacen/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from datetime import datetime
+from .models import Product
 
-def index(request):
+def home(request):
     return HttpResponse('👻 Bienvenido a mi aplicación con Django')
+
+def get_date(request):
+    now = datetime.now()
+    html = f'<h1>Fecha y hora actuales: {now}</h1>'
+    return HttpResponse(html)
+
+def get_json(request):
+    user = {
+        'id': 1,
+        'name': 'John Doe',
+        'email': 'john@example.com',
+        'age': 30
+    }
+    return JsonResponse(user)
+
+def get_html(request):
+    products = Product.objects.all()
+    return render(request, 'index.html', {'products': products})
